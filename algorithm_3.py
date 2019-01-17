c751f1d2a1c5d9ef4ea31fbfbb4de912ac5b7b87
Rainbow0625 <347601193@qq.com>
2019-01-17 22:24:25 +0800

    +.

--- algorithm_3.py
+++ algorithm_3.py
@@ -10,14 +10,14 @@ R_minHC = 0.001
 QR_minSC = 0.5
 QR_minHC = 0.001
 DEGREE = [R_minSC, R_minHC, QR_minSC, QR_minHC]
+Max_rule_length = 3  # not include head atom
 
-# embedding model parameters    should be modify!!!!!!!!!
+# embedding model parameters
 work_threads = 5
 nbatches = 150
 margin = 1  # the margin for the loss function
-
 train_times = 100  # 1000
-dimension = 50  #50
+dimension = 50  # 50
 alpha = 0.01  # learning rate
 lmbda = 0.1  # degree of the regularization on the parameters
 bern = 1  # set negative sampling algorithms, unif(0) or bern(1)
@@ -27,8 +27,8 @@ model = TransE.TransE
 # Unknown: TransD.TransD TransH.TransH TransR.TransR
 # Matrix: RESCAL.RESCAL
 
-
-def save_rules(BENCHMARK, nowPredicate, candidate, model, pre):
+# not changed yet.
+def save_rules(nowPredicate, candidate, pre):
     print("\nThe final rules are:")
     i = 1
     f = open('./rule/' + BENCHMARK + '/rule_' + str(model)[15:21] + '.txt', 'a+')
@@ -55,53 +55,55 @@ def save_rules(BENCHMARK, nowPredicate, candidate, model, pre):
     f.close()
 
 
-begin = time.time()
-print("\nThe benchmark is " + BENCHMARK + ".\n")
-with open('./benchmarks/' + BENCHMARK + '/relation2id.txt', 'r') as f:
-    # list: eg: "/location/country/form_of_government	0"
-    predicateSize = int(f.readline())
-    predicateName = [relation.split("	")[0] for relation in f.readlines()]
-    # print(predicateName)
-    print("Total predicates:" + str(predicateSize))
-num_rule = 0
-total_time = 0
-# 0:matrix 1:vector
-for Pt in range(predicateSize):
-    Pt_0 = time.time()
-    ent_size_all, nowPredicate = s.sample0(BENCHMARK, Pt, predicateName)
-    # The parameter of model should be adjust to the best parameters!!!!!!
-    ent_emb, rel_emb = te.trainModel(1, BENCHMARK, work_threads, train_times, nbatches, dimension, alpha, lmbda, bern,
-                                     margin, model)
-    _p, pre = rsalw.get_pre(BENCHMARK)
-    candidate = rsalw.searchAndEvaluate(1, BENCHMARK, nowPredicate, ent_emb, rel_emb,
-                                        dimension, ent_size_all, pre, DEGREE)
-    save_rules(BENCHMARK, nowPredicate, candidate, model, pre)
-    num_rule = num_rule + len(candidate)
-    Pt_1 = time.time()
-    Pt_time = Pt_1 - Pt_0
-    print("Predicate target's time: " + str(Pt_time) + "\n")
-    total_time = total_time + Pt_time
-    print("Average's time: " + str(total_time / (Pt + 1)) + "\n")
-    break
+if __name__ == '__main__':
+    begin = time.time()
+    print("\nThe benchmark is " + BENCHMARK + ".\n")
+    with open('./benchmarks/' + BENCHMARK + '/relation2id.txt', 'r') as f:
+        predicateSize = int(f.readline())
+        predicateName = [relation.split("	")[0] for relation in f.readlines()]
+        print("Total predicates:" + str(predicateSize))
+    num_rule = 0
+    total_time = 0
+    # 0:matrix 1:vector
+    for Pt in range(predicateSize):
+        # 对于每个规则长度进行循环!
+        Pt_0 = time.time()
+        for i in range(Max_rule_length):
 
-f = open('./rule/' + BENCHMARK + '/rule_After_' + str(model)[15:21] + '.txt', 'a+')
-f.write("Embedding parameter:\n")
-f.write("model: %s\n" + str(model))
-f.write("train_times: %d\n" % train_times)
-f.write("dimension: %d\n" % dimension)
-f.write("alpha: %f\n" % alpha)
-f.write("lmbda: %f\n" % lmbda)
-f.write("bern: %f\n" % bern)
-f.write("\nR_minSC:%f, R_minHC:%f\n" % (R_minSC, R_minHC))
-f.write("QR_minSC:%f, QR_minHC:%f\n" % (QR_minSC, QR_minHC))
-# f.write("syn: " + str(times_syn) + "\n")
-# f.write("coocc: " + str(times_coocc) + "\n")
-f.write("Total number of rules: %d\n" % num_rule)
-f.write("Average time:%s\n" % str(total_time/int(predicateSize)))
-f.close()
-end = time.time()-begin
-hour = int(end/3600)
-minute = int((end-hour*3600)/60)
-second = end-hour*3600-minute*60
-print("\nAlgorithm total time: ", end)
-print(str(hour)+" : "+str(minute)+" : "+str(second))
+            ent_size_all, nowPredicate = s.sample0(BENCHMARK, Pt, predicateName)
+            # The parameter of model should be adjust to the best parameters!!!!!!
+            ent_emb, rel_emb = te.trainModel(1, BENCHMARK, work_threads, train_times, nbatches, dimension, alpha, lmbda, bern,
+                                             margin, model)
+            _p, pre = rsalw.get_pre(BENCHMARK)
+            candidate = rsalw.searchAndEvaluate(1, BENCHMARK, nowPredicate, ent_emb, rel_emb,
+                                                dimension, ent_size_all, pre, DEGREE)
+            save_rules(nowPredicate, candidate, pre)
+            num_rule = num_rule + len(candidate)
+        Pt_1 = time.time()
+        Pt_time = Pt_1 - Pt_0
+        print("Predicate target's time: " + str(Pt_time) + "\n")
+        total_time = total_time + Pt_time
+        print("Average's time: %f\n" % (total_time / (Pt + 1)))
+        break
+
+    f = open('./rule/' + BENCHMARK + '/rule_After_' + str(model)[15:21] + '.txt', 'a+')
+    f.write("Embedding parameter:\n")
+    f.write("model: %s\n" + str(model))
+    f.write("train_times: %d\n" % train_times)
+    f.write("dimension: %d\n" % dimension)
+    f.write("alpha: %f\n" % alpha)
+    f.write("lmbda: %f\n" % lmbda)
+    f.write("bern: %f\n" % bern)
+    f.write("\nR_minSC:%f, R_minHC:%f\n" % (R_minSC, R_minHC))
+    f.write("QR_minSC:%f, QR_minHC:%f\n" % (QR_minSC, QR_minHC))
+    # f.write("syn: " + str(times_syn) + "\n")
+    # f.write("coocc: " + str(times_coocc) + "\n")
+    f.write("Total number of rules: %d\n" % num_rule)
+    f.write("Average time:%s\n" % str(total_time/int(predicateSize)))
+    f.close()
+    end = time.time()-begin
+    hour = int(end/3600)
+    minute = int((end-hour*3600)/60)
+    second = end-hour*3600-minute*60
+    print("\nAlgorithm total time: ", end)
+    print(str(hour)+" : "+str(minute)+" : "+str(second))
---
+++ sampling_3.py
@@ -0,0 +1,142 @@
+import scipy.io as sio  # related with .mat
+import numpy as np
+import time
+
+
+def readData(BENCHMARK):  # index from 0
+    # read the Fact.txt: h t r
+    with open('./benchmarks/' + BENCHMARK + '/Fact.txt', 'r') as file:
+        factSize = file.readline()
+        print("Total facts:" + factSize)
+        facts = np.array([line.strip('\n').split(' ') for line in file.readlines()], dtype='int32')
+        # print(facts)
+    with open('./benchmarks/' + BENCHMARK + '/entity2id.txt', 'r') as entityfile:
+        entitysize = entityfile.readline()
+        print("Total entities:" + str(entitysize))
+    return facts, int(entitysize)
+
+
+def sample(BENCHMARK, Pt, predicateName):
+    time_start = time.time()
+
+    facts, entitysize = readData(BENCHMARK)
+
+    # Sampling the Ei and Fi, i<lenOfRules
+    # input: facts, Pt, lenOfRules
+    lenOfRules = 2
+    print("\nStep1: get the E0")
+
+    F0ofPt = []
+    E0ofPt = set()
+    curPredicate = []
+    print("Pt's index:" + str(Pt) + "  " + str(predicateName[Pt]))
+    curPredicate.append(Pt)  # it will change
+    curPredicate.append(predicateName[Pt])
+    for f in facts:
+        fact = f.tolist()
+        # return the entities and facts related to certain entity
+        # just for the Ei-1 to Ei
+        if f[2] == Pt:
+            F0ofPt.append(fact)
+            if f[0] not in E0ofPt:
+                E0ofPt.add(f[0])
+            if f[1] not in E0ofPt:
+                E0ofPt.add(f[1])
+    factsSizeOfPt = len(F0ofPt)
+    entSizeOfPt = len(E0ofPt)
+    print("FactsPt 0 size: " + str(factsSizeOfPt))  # 35088
+    print("E0ofPt size: " + str(entSizeOfPt))  # 45709
+
+    time_1 = time.time()
+    print(curPredicate)
+    print('Step 1 cost time:', time_1-time_start)
+
+    E1ofPt = set()
+    predSampled = set()
+    F1ofPt = []
+    outFlag = 0
+    flag = 0
+    print("\nStep2: get the E1")
+    for fact in facts:
+        if fact[0] in E0ofPt:
+            E1ofPt.add(fact[1])  # set can not add the same element
+            predSampled.add(fact[2])
+            flag = 1
+        if fact[1] in E0ofPt:
+            E1ofPt.add(fact[0])
+            predSampled.add(fact[2])
+            flag = 1
+        f = fact.tolist()
+        if flag == 1 and f not in F0ofPt:
+            F1ofPt.append(f)
+        flag = 0
+        outFlag = outFlag+1
+        if outFlag % 100000 == 0:
+            print(outFlag)
+            print(f)
+    print("F1ofPt size: " + str(len(F1ofPt)))
+    print("E1ofPt size: " + str(len(E1ofPt)))
+    print("Predicates after sampled size:" + str(len(predSampled)))
+    time_2 = time.time()
+    print('Step 2 cost time:', time_2 - time_1)
+
+    print("\nStep3: get the Union, recode the index and write into file.")
+    EofPt = E0ofPt | E1ofPt  # EofPt entity
+    F0ofPt.extend(F1ofPt)  # F0ofPt facts
+    # predSampled predicates
+
+    # entity
+    Entity = np.array(list(EofPt))
+    f = open('./sampled/' + BENCHMARK + '/entity2id.txt', 'w')
+    entSizeOfPt = len(EofPt)
+    f.write(str(entSizeOfPt) + "\n")  # after sampling
+    print("EntityOfPt size: " + str(entSizeOfPt))
+    for x in range(entSizeOfPt):
+        f.write(str(x) + "\n")
+    f.close()
+
+    # predicate
+    f = open('./sampled/' + BENCHMARK + '/relation2id.txt', 'w')
+    predSampledli = list(predSampled)
+    predSize = len(predSampled)
+    nowPredicate = []
+    f.write(str(predSize*2) + "\n")  # after sampling
+    print("Predicated size: " + str(predSize*2))
+
+    #  name ok!
+    for i in range(predSize):
+        name = predicateName[predSampledli[i]]
+        # predSampledli[i] is the old index!
+        f.write(str(2*i) + " " + str(name) + " " + str(predSampledli[i]) + "\n")
+        f.write(str(2*i+1) + " " + str(name) + "^-1 " + str(predSampledli[i]) + "\n")
+        if curPredicate[1] == name:
+            nowPredicate = [i, name]
+    Predicate = np.array(predSampledli)
+    # print(predSampled)
+    # print(predSampledli)
+    # print(Predicate)
+    f.close()
+
+    # fact
+    Fact = []
+    for f in F0ofPt:
+        Fact.append([int(np.argwhere(Entity == f[0])), int(np.argwhere(Entity == f[1])),
+                     int(np.argwhere(Predicate == f[2]))*2])
+        Fact.append([int(np.argwhere(Entity == f[1])), int(np.argwhere(Entity == f[0])),
+                     int(np.argwhere(Predicate == f[2]))*2+1])
+    f = open('./sampled/' + BENCHMARK + '/Fact.txt', 'w')
+    factsSizeOfPt = len(Fact)
+    f.write(str(factsSizeOfPt) + "\n")
+    print("FactsOfPt size: " + str(factsSizeOfPt))
+    for line in Fact:
+        f.write(" ".join(str(letter) for letter in line) + "\n")
+    f.close()
+
+    time_3 = time.time()
+    print('Step 3 cost time:', time_3 - time_2)
+
+    time_end = time.time()
+    print('\nTotally cost time:', time_end - time_start)
+    print("Pt: ")
+    print(nowPredicate)
+    return entitysize, nowPredicate
